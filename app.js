b516938937650f295ef2039ab554e8b7e4999014
ElenBadalyanJS <elen.badalyan07@gmail.com>
2025-03-02 16:19:17 +0400

    adding myPop method under hoog

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 // console.log("try it works or not");
-Array.prototype.myPush = function (...args) {
-  for (let i = 0; i < args.length; i++) {
-    this[this.length] = args[i];
-  }
+// Array.prototype.myPush = function (...args) {
+//   for (let i = 0; i < args.length; i++) {
+//     this[this.length] = args[i];
+//   }
+// };
+// const array = [1, 5, 6, 7];
+// array.myPush(3, 5, 6);
+// console.log(array);
+
+Array.prototype.myPop = function () {
+  let lastItem = this[this.length - 1];
+  this.length = this.length - 1;
+  return lastItem;
 };
-const array = [1, 5, 6, 7];
-array.myPush(3, 5, 6);
-console.log(array);
+const array = [5, 10];
+let x = array.myPop();
+console.log(x, array);
