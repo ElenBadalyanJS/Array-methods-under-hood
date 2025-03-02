6efdd799c53571c36e333beef500384651e1bda6
ElenBadalyanJS <elen.badalyan07@gmail.com>
2025-03-02 16:10:43 +0400

    added my push method under hood

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("try it works or not");
+// console.log("try it works or not");
+Array.prototype.myPush = function (...args) {
+  for (let i = 0; i < args.length; i++) {
+    this[this.length] = args[i];
+  }
+};
+const array = [1, 5, 6, 7];
+array.myPush(3, 5, 6);
+console.log(array);
