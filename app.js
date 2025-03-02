646ba1d0f4c78dd8606981e341e5c4991dec0aa6
ElenBadalyanJS <elen.badalyan07@gmail.com>
2025-03-02 16:27:31 +0400

    added myIndexOf method

--- app.js
+++ app.js
@@ -8,11 +8,23 @@
 // array.myPush(3, 5, 6);
 // console.log(array);
 
-Array.prototype.myPop = function () {
-  let lastItem = this[this.length - 1];
-  this.length = this.length - 1;
-  return lastItem;
+// Array.prototype.myPop = function () {
+//   let lastItem = this[this.length - 1];
+//   this.length = this.length - 1;
+//   return lastItem;
+// };
+// const array = [5, 10];
+// let x = array.myPop();
+// console.log(x, array);
+Array.prototype.myIndexOf = function (item) {
+  let result = -1;
+  for (let i = 0; i < this.length; i++) {
+    if (this[i] === item) {
+      result = i;
+      break;
+    }
+  }
+  return result;
 };
-const array = [5, 10];
-let x = array.myPop();
-console.log(x, array);
+const array = [5, 6, 7, 8, 5, 9, 6, 10];
+console.log(array.myIndexOf(14));
