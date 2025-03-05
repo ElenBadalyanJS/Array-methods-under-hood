e64ce8fc8cb26dff197a855a50b856da97ffad77
ElenBadalyanJS <elen.badalyan07@gmail.com>
2025-03-05 19:39:41 +0400

    myMap

--- app.js
+++ app.js
@@ -16,15 +16,35 @@
 // const array = [5, 10];
 // let x = array.myPop();
 // console.log(x, array);
-Array.prototype.myIndexOf = function (item) {
-  let result = -1;
+// Array.prototype.myIndexOf = function (item) {
+//   let result = -1;
+//   for (let i = 0; i < this.length; i++) {
+//     if (this[i] === item) {
+//       result = i;
+//       break;
+//     }
+//   }
+//   return result;
+// };
+// const array = [5, 6, 7, 8, 5, 9, 6, 10];
+// console.log(array.myIndexOf(14));
+
+Array.prototype.myMap = function (callback) {
+  let newArr = [];
   for (let i = 0; i < this.length; i++) {
-    if (this[i] === item) {
-      result = i;
-      break;
-    }
+    let mapedItem = callback(this[i], i, this);
+    newArr.push(mapedItem);
   }
-  return result;
+  return newArr;
 };
-const array = [5, 6, 7, 8, 5, 9, 6, 10];
-console.log(array.myIndexOf(14));
+let array = [10, 20, 30];
+let array2 = array.myMap((el) => el * 10);
+
+// Array.prototype.myForEach = function (callback) {
+//   for (let i = 0; i < this.length; i++) {
+//     callback(this[i], i, this);
+//   }
+// };
+// let array = [10, 20, 30];
+// let sum = 0;
+// array.myForEach((el) => (sum = sum + el));
